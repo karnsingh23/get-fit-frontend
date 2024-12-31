d5ee3434875167f9e7f5fe15c928169a6f8235d6
Karan Singh <urskaransingh4@gmail.com>
2024-12-31 10:20:30 +0530

    tailwind problem fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}", // Adjust as needed
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
